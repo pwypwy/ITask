0941341ab53f7cfba891885e47146a20a74c97c5
pwy <974834857@qq.com>
2020-05-29 13:54:05 +0800

    配置文件

--- itask/src/main/java/com/panwy/itask/controller/Hello.java
+++ itask/src/main/java/com/panwy/itask/controller/Hello.java
@@ -1,11 +1,12 @@
 package com.panwy.itask.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
-    @GetMapping("/hello")
+    @RequestMapping("/hello")
     String test(){
         return "hello";
     }
